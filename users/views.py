5c391168bde15c19e74b24b28b916d1fc9000d7d
shizuAnkaiCat777 <srpedroyshisuk1@gmail.com>
2025-03-28 17:47:45 -0300

    nova linha de configuração pra auth, CRUD Users funcionando com token

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
@@ -23,6 +23,7 @@ class RegisterView(APIView):
 
 # =====================================================================================
 class LoginView(APIView):
+    permission_classes = [AllowAny] 
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
